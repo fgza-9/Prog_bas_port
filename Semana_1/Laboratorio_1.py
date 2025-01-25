0ba768d493cf9926c69ff79bc5cfcef0ed484f5b
fgza-9 <fer_garza9@hotmail.com>
2025-01-25 08:43:16 -0600

    Laboratorio_1

---
+++ Semana_1/Laboratorio_1.py
@@ -0,0 +1,52 @@
+'''
+# Ejercicio 1
+    ¿Cual es la funcioon de la memoria secundaria en una computadora?
+        c) Almacenar informacion durante mucho tiempo, incluso despues de ciclos de apagado
+        y encendido.
+# Ejercicio 2
+    ¿Que es un programa?
+        Un conjunto de instrucciones que le dice a la computadora que hacer.
+# Ejercicio 3
+    ¿Cual es la diferencia entre un compilador y un interprete?
+        un compilador traduce directamente todo el codigo, mientras que un interprete lo hace
+        linea por linea, siendo mas lento a la hora de ejecutar el codigo.
+# Ejercicio 4
+    ¿Cual de los siguientes contiene “codigo maquina”?
+        a) El interprete de Python.
+# Ejercicio 5
+    ¿Que esta mal en el siguiente codigo?:
+    >>> primt '¡Hola, mundo!'
+    File "<stdin>", line 1
+    primt '¡Hola, mundo!'
+        primt esta mal escrito, deberia ser print. Y las comillas debben ser dobles, o simples.
+# Ejercicio 6
+    ¿En que lugar del computador queda almacenada una variable, como en este caso “x”,
+    despues de ejecutar la siguiente lınea de Python?:
+    x = 123
+        b) Memoria Principal.
+# Ejercicio 7
+    ¿Que mostrara en pantalla el siguiente programa?:
+    x = 43
+    x = x + 1
+    print(x)
+        b)44
+# Ejercicio 8
+    Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad
+    humana:
+    1. Unidad central de procesamiento.
+        El cerebro, es el responsable de procesar las acciones y pensamientos
+    2. Memoria principal.
+        La capacidad de tener por un momento la informacion en la mente, o memoria a corto plazo.
+    3. Memoria secundaria.
+        Los recuerdos que tenemos del pasado, todo lo que almacena nuestra memoria a largo plazo.
+    4. Dispositivos de entrada.
+        Nuestro equivalente son los sentidos, asi como la vista, con ellos identificamos las cosas.
+    5. Dispositivos de salida.
+        Nuestras manos, y nuestra voz, con elllas realizamos y decimos acciones
+# Ejercicio 9
+    ¿Como puedes corregir un “Error de sintaxis”?
+        Revisando la linea donde se encuentra el error, buscando la estructura correcta y 
+        comparar con el error para asi llegar a la solucion.
+
+
+'''
